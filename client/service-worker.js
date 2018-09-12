b64433ecefd50cc6aff1d0b1788814a1e5958558
Viktor Ohad <ygatravaz@protonmail.com>
2018-09-12 06:44:40 +0200

    ex-07 complete

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,15 +1,38 @@
+let fallbackImage = 'generic-fallback-image';
+
+const FALLBACK_IMAGE_URL = 'https://localhost:3100/images/fallback-grocery.png';
+
 self.addEventListener('install', event => {
-  console.log('Installing service worker', event);
+  event.waitUntil(
+    caches.open(fallbackImage).then(cache => {
+      cache.add(FALLBACK_IMAGE_URL);
+    })
+  );
 });
 
 self.addEventListener('active', event => {
+  //TODO delete cache
   console.log('Service worker active!', event);
 });
 
-let count = 0;
+self.addEventListener('fetch', fetchEvent => {
+  let acceptHeaders = fetchEvent.request.headers.get('accept');
+  let reqUrl = new URL(fetchEvent.request.url);
 
-self.addEventListener('fetch', event => {
-  console.log('Intercepting fetch:', event);
-  count++;
-  console.log(`Number ${count}`);
+  if (acceptHeaders.indexOf('image/*') >= 0 && reqUrl.pathname.indexOf('/images/' === 0)) {
+    fetchEvent.respondWith(serveImage(fetchEvent));
+  }
 });
+
+function serveImage(fetchEvent) {
+  //TODO make an async function solution as well
+  return fetch(fetchEvent.request, { mode: 'cors' })
+    .then(response => {
+      if (!response.ok) {
+        return caches.match(FALLBACK_IMAGE_URL, { cacheName: fallbackImage });
+      } else {
+        return response;
+      }
+    })
+    .catch(() => caches.match(FALLBACK_IMAGE_URL, { cacheName: fallbackImage }));
+}
